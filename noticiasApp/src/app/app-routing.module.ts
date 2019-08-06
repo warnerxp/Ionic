b9e5a5c62c2545e60544d272d833a721ea93052f
Wagner Alvarado <warnerxp@outlook.com>
2019-08-06 17:35:04 -0600

    add page listado Noticias

--- noticiasApp/src/app/app-routing.module.ts
+++ noticiasApp/src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  }
+  },
+  { path: 'listado-noticias', loadChildren: './listado-noticias/listado-noticias.module#ListadoNoticiasPageModule' }
 ];
 
 @NgModule({
--- noticiasApp/src/app/app.component.ts
+++ noticiasApp/src/app/app.component.ts
@@ -12,6 +12,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   public appPages = [
     {
+      title: 'Listado Noticias',
+      url: '/listado-noticias',
+      icon: 'book'
+    },
+    {
       title: 'Home',
       url: '/home',
       icon: 'home'
