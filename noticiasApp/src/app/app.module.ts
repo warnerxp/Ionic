3bc3b4566f98637aeb86f1eec31f4f486ddfe72c
Wagner Alvarado <warnerxp@outlook.com>
2019-08-08 11:43:46 -0600

    Service Init
    Service Init

--- noticiasApp/src/app/app.module.ts
+++ noticiasApp/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NoticiasService } from './services/noticas-service/noticias.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,11 +17,13 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    NoticiasService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
