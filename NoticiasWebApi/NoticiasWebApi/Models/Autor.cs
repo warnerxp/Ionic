37e11794668577fe6d0fd5fac7f79fdd9a9975d9
Wagner Alvarado <warnerxp@outlook.com>
2019-08-09 09:33:45 -0600

    WebApi Notcias
    WebApi Notcias

---
+++ NoticiasWebApi/NoticiasWebApi/Models/Autor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoticiasWebApi.Models
+{
+    public class Autor
+    {
+        public int AutorID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public List<Noticia> Noticias { get; set; }
+
+    }
+}
